52cb01cece118000bd9651f7cdd18e58db144278
jian0078 <jane.jiang1023@gmail.com>
2018-05-18 21:19:19 -0400

    Finish Lab5

--- app/src/main/java/com/example/janej/andriodlabs/ChatWindowActivity.java
+++ app/src/main/java/com/example/janej/andriodlabs/ChatWindowActivity.java
@@ -34,8 +34,8 @@ public class ChatWindowActivity extends Activity {
     protected static final String ACTIVITY_NAME = "ChatWindow";
     ChatAdapter messageAdapter;
     Cursor cursor;
-
-
+    ChatDatabaseHelper databaseHelper;
+    SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,16 +47,45 @@ public class ChatWindowActivity extends Activity {
         messages = new ArrayList<>();
         listView.setAdapter(messageAdapter);
 
+        databaseHelper =new ChatDatabaseHelper(this);
+        db=databaseHelper.getWritableDatabase();
+        cursor= db.query(false,ChatDatabaseHelper.TABLE_NAME,new String[]{ChatDatabaseHelper.KEY_ID,ChatDatabaseHelper.KEY_MESSAGE},null,
+                null,null, null, null,null);
+
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()){
+            Log.i(ACTIVITY_NAME, "SQL MESSAGE:" + cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+            Log.i(ACTIVITY_NAME, "Cursor’s  column count =" + cursor.getColumnCount());// print an information message about the Cursor
+            String message= cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)); //print out the name of each column returned by the cursor to retrieve each column name.
+            messages.add(message);
+            cursor.moveToNext();
+        }
+        for(int i=0; i< cursor.getColumnCount();i++){
+            Log.i(ACTIVITY_NAME,"The column name is " +cursor.getColumnName(i));
+        }
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 messages.add(editText.getText().toString());
+                ContentValues cv =new ContentValues();
+                cv.put(ChatDatabaseHelper.KEY_MESSAGE,editText.getText().toString());
+                db.insert(ChatDatabaseHelper.TABLE_NAME, null,cv);
                 messageAdapter.notifyDataSetChanged();
                 editText.setText("");
             }
         });
 
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
+        databaseHelper.close();
+    }
+
         private class ChatAdapter extends ArrayAdapter<String> {
             public ChatAdapter(Context ctx) {
                 super(ctx, 0);
@@ -86,8 +115,6 @@ public class ChatWindowActivity extends Activity {
 
                 return result;
             }
-
-
         }
 
 
